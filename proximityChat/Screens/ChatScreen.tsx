5b798f9c8fd99ad3580e9cda7e63bb5b58e1fe1f
mbolduc <max.xam3000@gmail.com>
2024-04-15 16:26:10 -0400

    Envoyer messages

--- proximityChat/Screens/ChatScreen.tsx
+++ proximityChat/Screens/ChatScreen.tsx
@@ -4,6 +4,13 @@ import { Chat, MessageType, defaultTheme, darkTheme, Theme } from '@flyerhq/reac
 import { PreviewData } from '@flyerhq/react-native-link-preview';
 import * as ImagePicker from 'expo-image-picker';
 import Guid from 'guid';
+import { firebase } from '../firebaseconfig';
+
+type Message = MessageType.Any & {
+  parentid: string;
+  fromuserid: string;
+  touserid: string;
+}
 
 export default function ChatScreen(props) {
   const { navigation } = props;
@@ -13,22 +20,21 @@ export default function ChatScreen(props) {
       });
   }, [navigation]);
   const user = { id: '06c33e8b-e835-4736-80f4-63f44b66666c' } // TODO: user par firebase
-  const user2 = { id: '06c33e8b-e835-4736-80f4-63f34b66666c' } // TODO: user par firebase
-  const [messages, setMessages] = useState<MessageType.Any[]>([
-    {
-      author: user2,
-      createdAt: Date.now(),
-      id: Guid.raw(),
-      text: 'test',
-      type: 'text',
-    }
-
-  ]) // TODO: messages par firebase
+  const user2 = { id: '06c33e8b-e835-4736-80f4-63f34b66436c' } // TODO: user par firebase
+  const [messages, setMessages] = useState<MessageType.Any[]>([]) // TODO: messages par firebase
+  const messageRef = firebase.database().ref('messages');
 
   const colorScheme = Appearance.getColorScheme();
   const [theme, setTheme] = useState<Theme>(defaultTheme);
 
   function addMessage(message: MessageType.Any) {
+    messageRef.push({
+      ...message,
+      parentid: messages[0]?.id ?? '',
+      fromuserid: user.id,
+      touserid: user2.id,
+    })
+
     setMessages([message, ...messages]);
   }
 
@@ -102,7 +108,39 @@ export default function ChatScreen(props) {
     customTheme.borders.inputBorderRadius = 20;
 
     setTheme(customTheme);
-  }, [colorScheme])
+  }, [colorScheme]);
+
+  // useEffect(() => {
+
+  //   messageRef.on('child_added', (snapshot) => {
+  //     let message = snapshot.val();
+
+  //     if (message.author.id === user2.id) {
+  //       addMessage(message);
+  //     }
+  //   });
+  // }, [messageRef]);
+
+  useEffect(() => {
+    messageRef.once('value', (snapshot) => {
+      const ref = snapshot.val();
+      if (!ref) return;
+
+      const firebaseMessages = Object.entries(ref).map(x => x[1]) as Message[];
+      let currentMessages = []
+
+      let message: Message;
+      do {
+        message = firebaseMessages.find((m) => m.parentid === (message?.id ?? '')
+        && ((m.fromuserid === user2.id && m.touserid === user.id)
+        || (m.fromuserid === user.id && m.touserid === user2.id)));
+        if (!message) break;
+        currentMessages = [message as MessageType.Any, ...currentMessages]
+      } while (message)
+
+      setMessages(currentMessages);
+    });
+  }, []);
 
   return (
     <Chat
