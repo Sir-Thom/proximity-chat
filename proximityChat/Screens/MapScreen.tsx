4bb019c2ac3b5d9cbb4b35ca81d2c02bb98e45d2
Sir-Thom <102597615+Thomas-Toulouse-Cegep@users.noreply.github.com>
2024-05-02 13:38:06 -0400

    Update MapScreen.tsx

--- proximityChat/Screens/MapScreen.tsx
+++ proximityChat/Screens/MapScreen.tsx
@@ -1,16 +1,14 @@
-import * as Location from 'expo-location';
 import React, { useEffect, useState } from 'react';
-import { View, StyleSheet, Dimensions, Alert } from 'react-native';
+import { View, StyleSheet, Alert } from 'react-native';
 import MapView, { Marker, PROVIDER_GOOGLE } from 'react-native-maps';
 import { Asset } from 'expo-asset';
+import * as Location from 'expo-location';
 
 import { firebase } from '../firebaseconfig';
 import { getUserFirstnameById } from '../utils/GetUser';
 import { HandleLocataionUpdate, GetLocation } from '../utils/LocationsUtils';
 import { GTAMapStyle } from '../utils/mapStyle/GTAMapStyle';
 
-
-// Function to calculate the distance between two coordinates using Haversine formula
 export const calculateDistance = (lat1, lon1, lat2, lon2) => {
     const R = 6371; // Radius of the Earth in km
     const dLat = (lat2 - lat1) * (Math.PI / 180);
@@ -18,9 +16,9 @@ export const calculateDistance = (lat1, lon1, lat2, lon2) => {
     const a =
         Math.sin(dLat / 2) * Math.sin(dLat / 2) +
         Math.cos(lat1 * (Math.PI / 180)) *
-            Math.cos(lat2 * (Math.PI / 180)) *
-            Math.sin(dLon / 2) *
-            Math.sin(dLon / 2);
+        Math.cos(lat2 * (Math.PI / 180)) *
+        Math.sin(dLon / 2) *
+        Math.sin(dLon / 2);
     const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
     const distance = R * c; // Distance in km
     return distance;
@@ -54,7 +52,7 @@ const MapScreen = ({ navigation }) => {
 
         fetchLocation(); // Fetch location when the component mounts
 
-        const locationInterval = setInterval(fetchLocation, 60000); // Refresh location every 1 minute
+        const locationInterval = setInterval(fetchLocation, 10000); // Refresh location every 1 minute
 
         return () => clearInterval(locationInterval); // Clear interval on component unmount
     }, []);
@@ -63,13 +61,12 @@ const MapScreen = ({ navigation }) => {
         const fetchNearbyUsers = async () => {
             const locationData = await GetLocation();
             setNearbyUsers(locationData || []);
-            
         };
 
         fetchNearbyUsers(); // Fetch nearby users when the component mounts
 
         const nearbyUsersInterval = setInterval(fetchNearbyUsers, 10000); // Refresh nearby users every 1 minute
-      
+
         return () => clearInterval(nearbyUsersInterval); // Clear interval on component unmount
     }, []);
 
